448cffa8b821f307c6c9fc0b500e5e2d0c2bcafc
SeanCassone <77746228+SeanCassone@users.noreply.github.com>
2021-04-18 19:48:56 -0400

    added third component

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import classNames from "../utils/class-names";
 import useInterval from "../utils/useInterval";
 import { minutesToDuration, secondsToDuration } from "../utils/duration";
 import Session from "./Session";
+import Controls from "./Controls";
 
 function Pomodoro() {
   // Timer starts out paused
@@ -126,41 +126,12 @@ function Pomodoro() {
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <div
-            className="btn-group btn-group-lg mb-2"
-            role="group"
-            aria-label="Timer controls"
-          >
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-testid="play-pause"
-              title="Start or pause timer"
-              onClick={playPause}
-            >
-              <span
-                className={classNames({
-                  oi: true,
-                  "oi-media-play": !isTimerRunning,
-                  "oi-media-pause": isTimerRunning,
-                })}
-              />
-            </button>
-            {/* TODO: Implement stopping the current focus or break session and disable when there is no active session */}
-            <button
-              type="button"
-              className="btn btn-secondary"
-              title="Stop the session"
-              onClick={stopTimer}
-              disabled={!isSessionActive}
-            >
-              <span className="oi oi-media-stop" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <Controls
+        playPause={playPause}
+        stopTimer={stopTimer}
+        isTimerRunning={isTimerRunning}
+        isSessionActive={isSessionActive}
+      />
       <Session
         currentMode={currentMode}
         focusTime={focusTime}
